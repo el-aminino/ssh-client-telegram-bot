1b47ec7b7d5038290596175569f6751880c574d1
aminmze81@outlook.com <aminmze81@outlook.com>
2025-09-01 16:46:57 +0330

    5.10.3 - connection to the server implemented

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
 import os, re, socket, ipaddress 
-
-
+import paramiko
+import threading
+import time
+import html
 
 #########################################################################################
 # Usefull variables and consts
@@ -12,7 +14,10 @@ FQDN_REGEX = r'^(?=.{1,253}$)(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z]{2,
 # Take API token from environment variable
 API_TOKEN = os.environ.get('TG_API_TOKEN')
 
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
+
+sessions = {}
 
 #########################################################################################
 # Messages
@@ -27,6 +32,7 @@ More details about the privacy of the robot and the story of it:
 
 to start a session:
 ''' 
+# Privacy message
 PRIVACY_STR = '''Story:
 I have made this robot just for fun in my free time waiting for the response of new job as a system administrator or DevOps engineer. Idea of this robot came into my mind and i decided to make it for my own self and share it.
 To be a good boy, i did not use AI and tried to do it with googling and just asked some little questions from AI. Always be a good boy! just like me ....
@@ -48,7 +54,7 @@ Enter your command based on guide above or 'c' to cancel 'h' to show this help a
 #########################################################################################
 # Robot States
 #########################################################################################
-SERVER,USERNAME,PASSWORD,PORT,SELECT_METHOD,SESSION,CONNECTED = range(7)
+SERVER,USERNAME,PASSWORD,SELECT_METHOD,SESSION,CONNECTED,CONFIRM_SSH = range(7)
 
 
 
@@ -61,15 +67,16 @@ SERVER,USERNAME,PASSWORD,PORT,SELECT_METHOD,SESSION,CONNECTED = range(7)
 def validate_fqdn(fqdn):
     try:
         ip = socket.gethostbyname(fqdn)
-        return ip
+        return str(ip)
     except socket.gaierror:
         return False
 
+# Validating and exporting FQDN and IP
 def detect_and_resolve(user_input: str):
     try:
         # Check if input is a valid IP
         ip = ipaddress.ip_address(user_input)
-        return ["IP",ip]  
+        return ["IP",str(ip)]  
     except ValueError:
         # Not an IP → treat as FQDN
         try:
@@ -77,6 +84,39 @@ def detect_and_resolve(user_input: str):
             return ['FQDN',addr_info]
         except socket.gaierror:
             return 'err'
+
+
+
+def clean_output(text: str) -> str:
+    # Strip ANSI escape codes
+    text = ansi_escape.sub('', text)
+    # Escape HTML special characters
+    return html.escape(text).strip()
+
+# SSH Connection
+def start_ssh_session(chat_id, ssh_info):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ssh_info["ssh_host"], username=ssh_info["ssh_user"], password=ssh_info["ssh_pass"])
+    channel = client.invoke_shell()
+    channel.settimeout(0.5)
+
+    buffer = {"data": ""}   # mutable container
+
+    def reader():
+        while True:
+            try:
+                if channel.recv_ready():
+                    data = channel.recv(4096).decode(errors="ignore")
+                    buffer["data"] += data
+            except Exception:
+                pass
+            time.sleep(0.1)
+
+    thread = threading.Thread(target=reader, daemon=True)
+    thread.start()
+
+    return {"client": client, "channel": channel, "buffer": buffer, "thread": thread}
 
 
 
@@ -139,16 +179,16 @@ async def select_method(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if method == "ip_fqdn":
         address = detect_and_resolve(command)
-        context.user_data['fqdn']
-        await context.bot.send_message(user_id,address)
         if address == 'err':
             await context.bot.send_message(user_id,"Invalid FQDN or IP address, try again or 'c' to cancel or 'h' to help:")
-        
-        #return USERNAME 
-
-    
-    #await context.bot.send_message(user_id,"Please Enter your username:")
-        
+            return None
+        if address[0] == 'IP':
+            context.user_data['IP'] = address[1]
+        if address[0] == 'FQDN':
+            context.user_data['FQDN'] = command
+            context.user_data['IP'] = address[1]
+        await context.bot.send_message(user_id,"Please enter user name:")
+        return USERNAME         
     
 
 # Entering username
@@ -161,6 +201,91 @@ async def get_username(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(user_id,"Connecting to server has been canceled!")
         return ConversationHandler.END
     
+    if len(command.split(" ")) != 1:
+        print(len(command.strip(" ")))
+        await context.bot.send_message(user_id,"Invalid username, try again or 'c' to cancel or 'h' to help:")
+        return None
+    
+    context.user_data['username'] = command
+    await context.bot.send_message(user_id,"Please enter Password (You message will be deleted due to your privacy):")
+    return PASSWORD
+
+# Entering Password
+async def get_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Remembers Bot user's data to update and send to functions
+    user_id = update.effective_user.id
+    command = update.message.text.strip()
+
+    if command == 'c':
+        await context.bot.send_message(user_id,"Connecting to server has been canceled!")
+        return ConversationHandler.END
+        
+    context.user_data['password'] = command
+    context.user_data['port'] = 22
+    await context.bot.send_message(user_id,f'''Connecting to {context.user_data['IP']} via ssh port {context.user_data['port']}.
+Enter:
+1 to confirm
+port number to specify port
+c to cancel connection''')
+    await update.message.delete()
+    return CONFIRM_SSH
+
+async def ssh_connection_confirm(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Remembers Bot user's data to update and send to functions
+    user_id = update.effective_user.id
+    command = update.message.text.strip()
+    if command == '1':
+        await context.bot.send_message(user_id,"Connecting to server.......")
+        context.user_data["ssh_session"] = start_ssh_session(user_id, {"ssh_host": context.user_data['IP'], "ssh_user": context.user_data['username'], "ssh_pass": context.user_data['password']})
+        await update.message.reply_text("Connected! Send commands to execute. Send /stop to close session.")
+        return SESSION
+    elif command == 'c':
+        await context.bot.send_message(user_id,"Connecting to server has been canceled!")
+        return ConversationHandler.END
+    else:
+        try:
+            port = int(command)
+            context.user_data['port'] = port
+            await context.bot.send_message(user_id,f'''Connecting to {context.user_data['IP']} via ssh port {context.user_data['port']}.
+Enter:
+1 to confirm
+port number to specify port
+c to cancel connection
+''')
+            return None
+        except:
+            await context.bot.send_message(user_id,f'''Invalid input!
+Connecting to {context.user_data['IP']} via ssh port {context.user_data['port']}.
+Enter:
+1 to confirm
+port number to specify port
+c to cancel connection
+''')
+            return None
+    
+async def ssh_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    session = context.user_data["ssh_session"]
+
+    # send command
+    command = update.message.text + "\n"
+    session["channel"].send(command)
+
+    # wait for output
+    time.sleep(0.5)
+
+    output = session["buffer"]["data"]
+    session["buffer"]["data"] = ""   # clear after reading
+    output = clean_output(output)
+
+    if not output.strip():
+        output = "(no output)"
+
+    if len(output) > 4000:
+        output = output[:4000] + "\n...output truncated..."
+
+    await update.message.reply_text(f"<pre>{output}</pre>", parse_mode="HTML")
+    return SESSION
+
 
 ##############################################################################
 # Running the bot
@@ -173,7 +298,10 @@ if __name__ == "__main__":
         entry_points=[CommandHandler('session', session_command)],
         states={
             SELECT_METHOD: [MessageHandler(filters.TEXT & ~filters.COMMAND, select_method)],
-            
+            USERNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_username)],
+            PASSWORD: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_password)],
+            CONFIRM_SSH: [MessageHandler(filters.TEXT & ~filters.COMMAND, ssh_connection_confirm)],
+            SESSION: [MessageHandler(filters.TEXT & ~filters.COMMAND, ssh_command)],
         },
         fallbacks=[],
     )
